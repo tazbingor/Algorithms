cbb145a1202bc4f592a02d0d5908e049edc096bb
jieyan <852353298@qq.com>
2017-11-26 22:34:42 +0800

    commit reformat code

--- src/algorithms/chapter_1/BinarySearch.java
+++ src/algorithms/chapter_1/BinarySearch.java
@@ -1,28 +1,28 @@
 package chapter_1;
+
 import java.util.Arrays;
 import java.util.Scanner;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.In;
+
 
 public class BinarySearch {
 
     /**
+     * 二分查找
      *
-     * @param key
-     * @param a
-     * @return
+     * @param key 元素
+     * @param a 目标数组
+     * @return 下标
      */
-    public static int rank(int key,int[] a){
+    private static int rank(int key, int[] a) {
         int lo = 0;
         int hi = a.length - 1;
-        while (lo <=hi){
-            int mid = lo + (hi - lo) /2;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
 
-            if (key<a[mid])
-                hi = mid-1;
-            else if(key>a[mid])
-                lo = mid+1;
+            if (key < a[mid])
+                hi = mid - 1;
+            else if (key > a[mid])
+                lo = mid + 1;
             else
                 return mid;
         }
@@ -33,11 +33,11 @@ public class BinarySearch {
     public static void main(String[] args) {
 //            int[] whiteList = In.readInts(args[0]);//出现数组越界
 
-        int[] whiteList = { 1, 99, 3, 55, 44, 88, 25, 6, 4 };
+        int[] whiteList = {1, 99, 3, 55, 44, 88, 25, 6, 4};
         Arrays.sort(whiteList);
-        for (int i=0;i<whiteList.length;i++){
-            System.out.print(whiteList[i]+" ");
-        }
+
+        for(int i : whiteList)
+            System.out.print(i + " ");
 
         System.out.println("\n  请输入要查询的数");
         // 使用命令行参数
